701453099bf54deba60cdbae221f7ddac243ca2e
Kaung San Kyaw <kskthedeveloper@gmail.com>
2025-02-24 12:11:12 +1030

    added hasLoop without length

--- src/main/java/org/me/datastructure/linkedlist/findmiddle/LinkedList.java
+++ src/main/java/org/me/datastructure/linkedlist/findmiddle/LinkedList.java
@@ -106,6 +106,21 @@ public class LinkedList {
         return slow;
     }
 
+    public boolean hasLoop() {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 }
 
 
